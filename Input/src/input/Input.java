aa0bf991b171e97dabef5d8f243f0e5b29914a5f
ramirbm <ramirbm@011-4303-S23HK.students.cisd.conroeisd.loc>
2018-10-31 07:50:17 -0500

    Added period to end sentence.

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -33,7 +33,7 @@ public class Input {
           stringArr[3] = scan.nextLine();
           stringArr[4] = scan.nextLine();
           
-          out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2],
+          out.printf("%s %s %s %s %s.", stringArr[0], stringArr[1], stringArr[2],
                   stringArr[3], stringArr[4]);
           
           
